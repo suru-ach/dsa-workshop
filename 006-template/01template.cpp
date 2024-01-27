321bbc55adc5811763e57a24aa3d2240f1a73014
suru-ach <srujanacharya36@gmail.com>
2024-01-27 08:21:52 +0530

    fix pair

--- 006-template/01template.cpp
+++ 006-template/01template.cpp
@@ -2,20 +2,9 @@
 
 using namespace std;
 
-template<typename T>
-class Pair
-{
-    public:
-        Pair(T f, T s) {
-            first = f;
-            second = s;
-        }
-    
-        T first;
-        T second;
-};
-
 int main() {
-    Pair<int> p1(1,1);
-    cout << p1.first << " " << p1.second << endl;
+    /*
+        Pair<int, int> p1(1,1);
+        cout << p1.first << " " << p1.second << endl;
+    */
 }
